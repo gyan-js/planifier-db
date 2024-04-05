c32880bb6a7ae3ff15a67c9f9519ffecd145acaa
Gyan <gyan0304@gmail.com>
2024-04-05 02:57:18 -0400

    Created endpoint /fetchTaskByDate

--- routes/tasks.js
+++ routes/tasks.js
@@ -18,16 +18,16 @@ const validateBoolean = (value) => {
 
 router.post('/addTask', async (req, res) => {
     const { accessToken, 
-        taskId,
         taskName, 
-        taskDescrption, 
+        taskDescrption,
+        taskDate,
         startTime, 
         endTime, 
         isAlarm, 
         isImportant, 
         userName} = req.body
 
-        if( !validateBoolean(isAlarm) || !validateBoolean(isImportant) ) {
+        if(!validateBoolean(isAlarm) || !validateBoolean(isImportant) ) {
             res.status(400).json({message: 'isAlarm and isImportant must be boolean values'})
             return;
         }
@@ -36,7 +36,7 @@ router.post('/addTask', async (req, res) => {
                 const usersCollection = await connectToMongo('tasks')
                 await usersCollection.updateOne(
                     {accessToken},
-                    {$push: {tasks: {taskName, taskDescrption, startTime, endTime, isAlarm, isImportant, userName}}},
+                    {$push: {tasks: {taskName, taskDescrption, taskDate, startTime, endTime, isAlarm, isImportant, userName}}},
                     {upsert: true}
                 );
                 res.status(200).json({message: 'Task Added to DB'})
@@ -60,6 +60,32 @@ router.get('/fetchTasks', async (req, res) => {
         }
         else {
             res.status(404).json({message: 'No tasks found for this accessToken'})
+        }
+    }
+    catch (error) {
+        console.log("Error fetching tasks:", error)
+        res.status(400).json({message: 'Error fetching tasks'})
+    }
+})
+
+router.get('/fetchTasksByDate', async (req, res) => {
+    const {accessToken, taskDate} = req.body
+
+    try{
+        const taskCollection = await connectToMongo('tasks')
+        const user = await taskCollection.findOne({accessToken});
+
+        if(user) {
+            const taskWithSameDate = user.tasks.filter(task => task.taskDate === taskDate)
+            if(taskWithSameDate.length > 0) {
+                res.status(200).json(taskWithSameDate)
+            }
+            else{
+                res.status(404).json({message: `No tasks found for this date for accessToken  `})
+            }
+        }
+        else {
+            res.status(404).json({message: `No tasks found for this date for accessToken  `})
         }
     }
     catch (error) {

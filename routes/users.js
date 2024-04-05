2eac01ab43099b67877ad8d30ed0e6461d7b92ff
Gyan <gyan0304@gmail.com>
2024-04-05 02:36:10 -0400

    Major changes in the user's endpoints

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ router.post('/add/google', async (req, res) => {
         const usersCollection = await connectToMongo('users')
         await usersCollection.updateOne(
             {accessToken},
-            {$set: {userName, userEmail}},
+            {$push: {info: {userName, userEmail}}},
             {upsert: true}
         );
         res.status(200).json({message: 'User Added to DB'})
@@ -40,7 +40,7 @@ router.get('/fetch/google', async (req, res) => {
         const user = await usersCollection.findOne({accessToken});
 
         if(user) {
-            res.status(200).json(user)
+            res.status(200).json(user.info)
         }
         else {
             res.status(400).json({message: 'User not found'})
@@ -51,6 +51,48 @@ router.get('/fetch/google', async (req, res) => {
         res.status(400).json({message: 'Error fetching user'})
     }
 })
-//router.post('/add/email')
+
+router.post('/add/email', async (req, res) => {
+    const { 
+        userName, 
+        userEmail,
+        password
+          } = req.body
+
+    try{
+        const usersCollection = await connectToMongo('users')
+        await usersCollection.updateOne(
+            {userEmail},
+            {$push: {info: {userName, password}}},
+            {upsert: true}
+        );
+        res.status(200).json({message: 'User Added to DB'})
+    }
+    catch(error) {
+        console.log('Error adding user:', error)
+        res.status(400).json({message: 'Error adding user'})
+    }
+})
+
+router.get('/fetch/email', async (req, res) => {
+    const {userEmail} = req.body
+
+    try{
+        const usersCollection = await connectToMongo('users')
+        const user = await usersCollection.findOne({userEmail});
+
+        if(user) {
+            res.status(200).json(user.info)
+        }
+        else {
+            res.status(400).json({message: 'User not found'})
+        }
+    }
+    catch(error){
+        console.log('Error fetching user:', error)
+        res.status(400).json({message: 'Error fetching user'})
+    }
+})
+
 
 module.exports = router

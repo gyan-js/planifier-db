f8fe0eb7c1326f0a3a4b5ce38d7060b117c2177f
Gyan <gyan0304@gmail.com>
2024-04-10 04:15:54 -0400

    added the configuration endpoint route

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const taskRoute = require('./routes/tasks') 
 const userRoute = require('./routes/users')
+const configRoute = require('./routes/config')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -11,7 +12,7 @@ dotenv.config()
 
 
 app.use(bodyParser.json())
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.json({message: "Hello Planifier"})
 })
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/tasks', taskRoute)
 app.use('/api/users', userRoute)
-
+app.use('/api/config', configRoute)
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
